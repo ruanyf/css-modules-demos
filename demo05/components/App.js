322e9020619f36788ffc7b5dd80d1a72396984b6
ruanyf <yifeng.ruan@gmail.com>
2016-06-04 10:36:46 +0800

    Updates

---
+++ demo05/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import style from './App.css';
+
+export default () => {
+  return (
+    <h1 className={style.title}>
+      Hello World
+    </h1>
+  );
+};
---
+++ demo06/webpack.config.js
@@ -0,0 +1,28 @@
+var values = require('postcss-modules-values');
+
+module.exports = {
+  entry: __dirname + '/index.js',
+  output: {
+    publicPath: '/',
+    filename: './bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'stage-0', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader?modules!postcss-loader"
+      },
+    ]
+  },
+  postcss: [
+    values
+  ]
+};
